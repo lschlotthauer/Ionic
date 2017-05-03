c4a630cad2c0a8a1a9707a0faee494fbc46d2d12
Lucas Schlotthauer <Lucas Schlotthauer>
2017-05-03 00:12:44 -0300

    Sonido andando

--- Piano/piano/src/pages/home/home.ts
+++ Piano/piano/src/pages/home/home.ts
@@ -13,14 +13,29 @@ export class HomePage {
 
   constructor(public navCtrl: NavController, plt: Platform, private nativeAudio: NativeAudio, private vibration: Vibration) {
      plt.ready().then((readySource) => {
-       this.nativeAudio.preloadSimple('snare', '../assets/snare.mp3').then(this.onSuccess, this.onError);
+       this.nativeAudio.preloadSimple("snare", "assets/snare.mp3").then(this.onSuccess, this.onError);
+       this.nativeAudio.preloadSimple("bass", "assets/bass.mp3").then(this.onSuccess, this.onError);
+       this.nativeAudio.preloadSimple("bongo", "assets/bongo.mp3").then(this.onSuccess, this.onError);
+       this.nativeAudio.preloadSimple("highhat", "assets/highhat.mp3").then(this.onSuccess, this.onError);
     });
     
   }
 
-  play(){
-    this.vibration.vibrate(1000);
-    this.nativeAudio.play('snare').then(this.onSuccess, this.onError);
+  playSnare(){
+    this.nativeAudio.play("snare").then(this.onSuccess, this.onError);
+    this.vibration.vibrate(100);
+  }
+    playBass(){
+    this.nativeAudio.play("bass").then(this.onSuccess, this.onError);
+    this.vibration.vibrate(100);
+  }
+    playBongo(){
+    this.nativeAudio.play("bongo").then(this.onSuccess, this.onError);
+    this.vibration.vibrate(100);
+  }
+    playHighhat(){
+    this.nativeAudio.play("highhat").then(this.onSuccess, this.onError);
+    this.vibration.vibrate(100);
   }
 
   onSuccess(){
