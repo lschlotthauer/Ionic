61ac4c963d755206aaec37050258f5d713bd10c0
Lucas Schlotthauer <Lucas Schlotthauer>
2017-05-05 00:11:10 -0300

    Imagenes

--- Piano/piano/src/app/app.module.ts
+++ Piano/piano/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { LoginPage } from '../pages/login/login';
@@ -18,7 +17,6 @@ import { Vibration } from '@ionic-native/vibration';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     TabsPage,
     LoginPage
@@ -31,7 +29,6 @@ import { Vibration } from '@ionic-native/vibration';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     TabsPage,
     LoginPage
